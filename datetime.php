b7755ddc45efdd5b5185cc659b24a3d156c51dd5
vetri <vetrichelvi26@gmail.com>
2024-04-06 19:31:01 +0530

    first commit

---
+++ datetime.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dateand time</title>
+</head>
+
+<body>
+    <button type="button" onclick="document.getElementById('demo').innerHTML = Date()">
+        Click me to display Date and Time.</button>
+    <p id="demo"></p>
+
+</body>
+
+</html>
