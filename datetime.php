a9207dcdc3120dfe3614b89c364501d7edc2ecfc
vetri <vetrichelvi26@gmail.com>
2024-04-06 19:42:58 +0530

    add color

--- datetime.php
+++ datetime.php
@@ -6,8 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dateand time</title>
 </head>
+<style>
+    body {
+        background-color: DodgerBlue;
+
+    }
+</style>
 
 <body>
+
     <button type="button" onclick="document.getElementById('demo').innerHTML = Date()">
         Click me to display Date and Time.</button>
     <p id="demo"></p>
